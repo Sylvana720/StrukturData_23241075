192c649de72d20e96181613aee2929ed4545e1da
Sylvana720 <sylva.baiq72@gmail.com>
2024-06-26 11:29:27 +0700

    Add files via upload

---
+++ meet10.py
@@ -0,0 +1,25 @@
+# Belajar Perbandingan Lanjutan (ELIF)
+
+nama = input("Masukkan Nama : ")
+
+if nama == "Edy":
+    print("Dosen Ganteng")
+elif nama == "Haryanto":
+    print("Dosen Ganteng banget")
+elif nama == "Ika":
+    print("Lah? Kan ini Mahasiswi :v")
+elif nama == "Sylvana":
+    print("Eh ternyata si penulis script :v")
+elif nama == "Vani":
+    print("Lah ini adek kau woy, beda jurusan :v")
+elif nama == "Gail":
+    print("Lah ini abang kau kocak, sama juga kek adik lu")
+elif nama == "Kucing":
+    print("Ini binatang bukan orang -_-")
+elif nama == "Milkman":
+    print("...bro... *emot batu*")
+else:
+    print("...who the fuck are you? F|")
+print("    ")
+print("Pencarian selesai")
+
